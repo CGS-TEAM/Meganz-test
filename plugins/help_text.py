41a900dba6de5b4caa2c9dcdcf19b12ca2e9971d
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2021-12-06 21:26:22 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -28,7 +28,7 @@ REPLY_MARKUP = InlineKeyboardMarkup(
     ]
 )
 
-@Client.on_message(filters.command("help"))
+@Client.on_message(filters.command("jxjdjsjjlp"))
 async def help_user(bot, update):
     fuser = update.from_user.id
     if check_blacklist(fuser):
@@ -44,7 +44,7 @@ async def help_user(bot, update):
         reply_to_message_id=update.message_id
     )
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("hdhdht"))
 async def start(bot, update):
     fuser = update.from_user.id
     if check_blacklist(fuser):
