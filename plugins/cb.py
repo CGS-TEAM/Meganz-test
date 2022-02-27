98972ad671522315d1cc578936721559bed467d8
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-02-27 19:16:58 +0530

    Update cb.py

--- plugins/cb.py
+++ plugins/cb.py
@@ -1,4 +1,5 @@
 import os
+import config
 from translation import Translation
 from pyrogram import Client as app
 from pyrogram.errors import UserNotParticipant
@@ -10,19 +11,61 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def cb_data(client, message):
     if message.data == "jsjsjp":
         await message.message.edit_text(
-            text=exe.HELP_TEXT.format(message.from_user.mention),
-            reply_markup=exe.btns,
+            text=exe.HELP_USER.format(message.from_user.mention),
+            reply_markup=Translation.btns,
             disable_web_page_preview=True,
         )
     elif message.data == "helpback":
         await message.message.edit_text(
-            text=exe.START_TEXT.format(message.from_user.mention),
+            text=Translation.START_TEXT.format(message.from_user.mention),
             reply_markup=exe.btn,
             disable_web_page_preview=True
         )
     elif message.data == "help":
         await message.message.edit_text(
-            text=exe.HELP_TEXT.format(message.from_user.mention),
+            text=Translation.HELP_USER.format(message.from_user.mention),
             reply_markup=exe.btns,
             disable_web_page_preview=True
         )
+    elif message.data == "refreshme":
+        if config.UPDATES_CHANNEL:
+            invite_link = await client.create_chat_invite_link(int(config.UPDATES_CHANNEL))
+            try:
+                user = await client.get_chat_member(int(config.UPDATES_CHANNEL), message.message.chat.id)
+                if user.status == "kicked":
+                    await message.message.edit(
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/CGSSUPPORT).",
+                        parse_mode="markdown",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await message.message.edit(
+                    text="<b>Hey</b> {},\n\n<b>You still didn't join our Updates Channel ☹️ \nPlease Join and hit on the 'Refresh 🔄' Button</b>".format(message.from_user.mention),
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("Join Our Updates Channel 🗣", url=invite_link.invite_link)
+                            ],
+                            [
+                                InlineKeyboardButton("Refresh 🔄", callback_data="refreshme")
+                            ]
+                        ]
+                    ),
+                    parse_mode="HTML"
+                )
+                return
+            except Exception:
+                await message.message.edit(
+                    text="Something went Wrong. Contact my [Support Group](https://t.me/CGSSUPPORT).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        await message.message.edit(
+            text=Translation.START_TEXT.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=Translation.btn,
+        )
+    else:
+        await message.message.delete()
