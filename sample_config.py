fa67b239907823ea1d98bfd222167100e83fe445
CGS TEAM <87460370+CGS-TEAM@users.noreply.github.com>
2022-02-27 20:16:48 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,6 +36,8 @@ class Config(object):
     #AUTH_USERS = set(int(x) for x in (id1, id2)) 👈 Type exactly as that and replace id1 and id2 with the id's of the telegram users, who you want to allow for multitasking. You cand add many users like that!
     
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    MONGO_URI = os.environ.get("MONGO_URI", "")
     DOWNLOAD_LOCATION = "./DOWNLOADS" # The download location for users. (Don't change anything in this field!)
     ADMIN_LOCATION = "./ADOWNLOADS" # The download location for auth users. (Don't change anything in this field!)
     CREDENTIALS_LOCATION = "./CREDENTIALS" # Location where your mega.nz credentials for megatools gets saved if you provide them. (Don't change anything in this field!)
